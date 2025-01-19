c72ec643173958285184ab9a20c0225a53905895
Takeshi Yu <hey@takeshiyu.me>
2025-01-19 20:41:54 +0800

    update footer

--- .vitepress/theme/Footer.vue
+++ .vitepress/theme/Footer.vue
@@ -1,6 +1,22 @@
 <template>
     <footer class="mt-16">
         <div class="relative">
+            <div class="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden flex-grow">
+                <div
+                    class="absolute left-1/2 bottom-0 ml-[-38rem] h-[15rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
+                    <div
+                        class="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_bottom,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100">
+                        <div
+                            class="my-pattern inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-0deg] fill-black/40 stroke-black/50 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/5">
+                        </div>
+                    </div>
+                    <svg viewBox="0 0 1113 440" aria-hidden="true"
+                        class="absolute left-1/2 top-0 ml-[-19rem] w-[69.5625rem] fill-white blur-[26px] dark:hidden">
+                        <path d="M.016 439.5s-9.5-300 434-300S882.516 20 882.516 20V0h230.004v439.5H.016Z"></path>
+                    </svg>
+                </div>
+            </div>
+
             <section class="text-center py-16 border-t border-slate-200 dark:border-slate-200/5">
                 <p class="mt-8 text-sm/6 text-zinc-500 md:order-1 md:mt-0">Copyright 2024 - 2025, Takeshi Yu.</p>
                 <p class="text-zinc-500 text-sm/6">The cool parts were build with <a href="https://vitepress.dev"
