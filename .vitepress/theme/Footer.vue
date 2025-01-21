f945969d4efab7d55e5a8a1d4b641f8ea9a5444b
Takeshi Yu <hey@takeshiyu.me>
2025-01-21 22:43:22 +0800

    update footer

--- .vitepress/theme/Footer.vue
+++ .vitepress/theme/Footer.vue
@@ -2,11 +2,11 @@
     <footer class="mt-16">
         <div class="relative">
             <section class="text-center py-16 border-t border-slate-200/5">
-                <p class="mt-8 text-sm/6 text-zinc-500 md:order-1 md:mt-0">Copyright 2024 - 2025, Takeshi Yu.</p>
-                <p class="text-zinc-500 text-sm/6">The cool parts were build with <a href="https://vitepress.dev"
-                        target="_blank" rrel="noopnener noreferrer"
-                        class="font-medium  hover:text-emerald-600 text-emerald-400">VitePress</a>
-                    and inspired by <a href="https://tailwindcss.com/blog" target="_blank" rel="noopnener noreferrer"
+                <p class="mt-8 text-sm/6 text-zinc-500 md:order-1 md:mt-0">Copyright 2024 - 2025, Takeshi Yu. Crafted
+                    with <a href="https://vitepress.dev" target="_blank" rrel="noopnener noreferrer"
+                        class="font-medium  hover:text-emerald-600 text-emerald-400">VitePress</a>.</p>
+                <p class="text-zinc-500 text-sm/6">The cool parts were inspired by <a
+                        href="https://tailwindcss.com/blog" target="_blank" rel="noopnener noreferrer"
                         class="font-medium  hover:text-emerald-600 text-emerald-400">Tailwind
                         CSS Blog</a> and <a href="https://tailwindui.com/templates/protocol" target="_blank"
                         rrel="noopnener noreferrer"
