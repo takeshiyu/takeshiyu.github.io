0df388a84b73f291034fb7983974fdc4525fa205
Takeshi Yu <hey@takeshiyu.me>
2025-02-14 18:26:33 +0800

    bug fix

--- .vitepress/theme/Author.vue
+++ .vitepress/theme/Author.vue
@@ -9,11 +9,10 @@ const { frontmatter } = useData()
         <ul class="flex flex-wrap text-sm leading-6 -mt-6 -mx-5">
             <li class="flex items-center font-medium whitespace-nowrap px-5 mt-6">
                 <img v-if="frontmatter.gravatar" :src="'https://gravatar.com/avatar/' + frontmatter.gravatar"
-                    alt="author image" class="mr-3 w-9 h-9 rounded-full bg-slate-50 dark:bg-slate-800"
-                    decoding="async" />
+                    alt="author image" class="mr-3 w-9 h-9 rounded-full bg-slate-800" decoding="async" />
 
                 <div class="text-sm leading-4">
-                    <div class="text-slate-900 dark:text-slate-200">{{ frontmatter.author }}</div>
+                    <div class="text-slate-200">{{ frontmatter.author }}</div>
                     <div class="mt-1">
                         <a :href="'https://twitter.com/' + frontmatter.twitter" target="_blank"
                             rel="noopnener noreferrer" class=" hover:text-emerald-600 text-emerald-400">{{
--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -8,28 +8,26 @@ import Header from './Header.vue'
         <Header />
         <div
             class="relative sm:pb-12 sm:ml-[calc(2rem+1px)] md:ml-[calc(3.5rem+1px)] lg:ml-[max(calc(14.5rem+1px),calc(100%-48rem))]">
-            <div
-                class="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800 sm:block">
+            <div class="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-800 sm:block">
             </div>
             <div class="space-y-16">
                 <article class="relative group" v-for="{ title, url, date, excerpt } of posts">
                     <div
-                        class="absolute -inset-y-2.5 -inset-x-4 md:-inset-y-4 md:-inset-x-6 sm:rounded-2xl group-hover:bg-zinc-50/70 dark:group-hover:bg-zinc-800/50">
+                        class="absolute -inset-y-2.5 -inset-x-4 md:-inset-y-4 md:-inset-x-6 sm:rounded-2xl group-hover:bg-zinc-800/50">
                     </div><svg viewBox="0 0 9 9"
-                        class="hidden absolute right-full mr-6 top-2 text-zinc-200 dark:text-zinc-600 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
-                        <circle cx="4.5" cy="4.5" r="4.5" stroke="currentColor" class="fill-white dark:fill-zinc-900"
-                            stroke-width="2"></circle>
+                        class="hidden absolute right-full mr-6 top-2 text-zinc-600 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
+                        <circle cx="4.5" cy="4.5" r="4.5" stroke="currentColor" class="fill-zinc-900" stroke-width="2">
+                        </circle>
                     </svg>
                     <div class="relative">
-                        <h3
-                            class="text-base font-semibold tracking-tight text-zinc-900 dark:text-zinc-200 pt-8 lg:pt-0">
+                        <h3 class="text-base font-semibold tracking-tight text-zinc-200 pt-8 lg:pt-0">
                             {{ title }}</h3>
-                        <div class="mt-2 mb-4 prose prose-zinc prose-a:relative prose-a:z-10 dark:prose-dark line-clamp-2"
+                        <div class="mt-2 mb-4 prose prose-zinc prose-a:relative prose-a:z-10 prose-dark line-clamp-2"
                             v-html="excerpt">
                         </div>
                         <dl class="absolute left-0 top-0 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
                             <dt class="sr-only">Date</dt>
-                            <dd class="whitespace-nowrap text-sm leading-6 dark:text-zinc-400"><time
+                            <dd class="whitespace-nowrap text-sm leading-6 text-zinc-400"><time
                                     :datetime="date.string">{{ date.string }}</time></dd>
                         </dl>
                     </div>
