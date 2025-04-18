f0ba6fee49528ec16a7a85e722df80b02d4bb2af
Takeshi Yu <hey@takeshiyu.me>
2025-04-18 14:41:03 +0800

    update footer

--- .vitepress/theme/Footer.vue
+++ .vitepress/theme/Footer.vue
@@ -5,15 +5,15 @@
                 <p class="mt-8 text-sm/6 text-zinc-500 md:order-1 md:mt-0">Copyright 2024 - 2025, Takeshi Yu. Crafted
                     with <a href="https://vitepress.dev" target="_blank" rrel="noopnener noreferrer"
                         class="font-medium  hover:text-emerald-600 text-emerald-400">VitePress</a>.</p>
-                <p class="text-zinc-500 text-sm/6">The cool parts were inspired by <a
+                <!-- <p class="text-zinc-500 text-sm/6">The cool parts were inspired by <a
                         href="https://tailwindcss.com/blog" target="_blank" rel="noopnener noreferrer"
                         class="font-medium  hover:text-emerald-600 text-emerald-400">Tailwind
                         CSS Blog</a> and <a href="https://tailwindui.com/templates/protocol" target="_blank"
                         rrel="noopnener noreferrer"
                         class="font-medium  hover:text-emerald-600 text-emerald-400">Tailwind
                         UI - Protocol.</a>
-                </p>
-                <p class="text-zinc-500 text-sm/6">The bad parts were made up by Takeshi.</p>
+                </p> -->
+                <!-- <p class="text-zinc-500 text-sm/6">The bad parts were made up by Takeshi.</p> -->
             </section>
         </div>
     </footer>
